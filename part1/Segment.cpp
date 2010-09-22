4a1794d1fe9d096830fa396d3efed3675221fca4
rstrong <rjstrong@gmail.com>
2010-09-21 22:21:17 -0700

    now counts the number of segments.  Numbers seem a tad high occasionally, but that might be due to using the YUV color space

--- part1/Segment.cpp
+++ part1/Segment.cpp
@@ -30,6 +30,8 @@ int main(int argc, char * argv[])
 	char **filename = &argv[1];
 	std::string fname(*filename);
 	std::cout << "Using filename: " << fname << std::endl;
+	std::string output;
+	output = "output.png";
 
   // Initialize ImageMagick install location for Windows
 	Magick::InitializeMagick(*argv);
@@ -37,6 +39,95 @@ int main(int argc, char * argv[])
 	try
 	{
 		Magick::Image image(fname);
+		image.segment(1.0, 1.5);
+		
+		Magick::Geometry size;
+		size = image.size();
+		int height = size.height();
+		int width = size.width();
+		std::cout << "Image size is: " << height << "x" << width << std::endl;
+	
+		int segments = 0;
+		int s[width][height];
+
+		for(int i = 0; i < height; i++)
+		{
+			for(int j = 0; j < width; j++)
+			{
+				s[i][j] = -1;
+			}
+		}
+
+		Magick::ColorYUV pixel;
+		double y,u,v;
+		double y1,u1,v1;
+		int h_min,h_max;
+		int w_min,w_max;
+		for(int i = 0; i < height; i++)
+		{
+			for(int j = 0; j < width; j++)
+			{
+				// Check all neighbors
+				h_min = i - 1;
+				h_max = i + 1;
+				if(h_min < 0)
+				{
+					h_min = 0;
+				}
+				if(h_max > (height -1))
+				{
+					h_max = height - 1;
+				}
+
+				w_min = j - 1;
+				w_max = j + 1;
+				if(w_min < 0)
+				{
+					w_min = 0;
+				}
+				if(w_max > (width -1))
+				{
+					w_max = width - 1;
+				}
+
+				int found = 0;
+				for(int n = h_min; n <= h_max; n++)
+				{
+					for(int m = w_min; m <= w_max; m++)
+					{
+		//				std::cout << "(" << i << "," << j << ") is now checking neighbor (" << n << "," << m << ")" << std::endl;
+						if(s[n][m] > 0)
+						{
+		//					std::cout << "(" << n << "," << m << ") has a segment" << std::endl;
+							pixel = image.pixelColor(i,j);
+							y = pixel.y();
+							u = pixel.u();
+							v = pixel.v();
+							pixel = image.pixelColor(n,m);
+							y1 = pixel.y();
+							u1 = pixel.u();
+							v1 = pixel.v();
+		//					std::cout << "comparing (" << y << "," << u << "," << v << ") with (" << y1 << "," << u1 << "," << v1 << ")" << std::endl;
+							if((y == y1) && (u == u1) && (v == v1))
+							{
+		//						std::cout << "Found! (" << n << "," << m << ") segment is" << s[n][m] << std::endl;
+								s[i][j] = s[n][m];
+								found = 1;
+							}
+						}
+					}
+				}
+				if(found == 0)
+				{
+					segments++;
+					s[i][j] = segments;
+		//			std::cout << "Not found, (" << i << "," << j << ") segment is " << s[i][j] << std::endl;
+				}
+			}
+		}
+
+		std::cout << "I found: " << segments << " segments" << std::endl;
+		image.write(output);
 	}
 	catch(Magick::Exception &error_)
 	{
@@ -45,3 +136,4 @@ int main(int argc, char * argv[])
 	}
 	return 0;
 }
+
