69568191d354bf76ce0e9f184009049ae0582e57
unknown <hardcore@.(none)>
2010-10-31 17:04:02 -0700

    updates now with proper pruning

--- part2-sift/part2-sift/Sifter.cpp
+++ part2-sift/part2-sift/Sifter.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <map>
+#include <utility>
 #include "Sifter.h"
 #include "Sift.h"
 #include <Magick++.h>
@@ -34,6 +36,10 @@ std::vector<std::vector<float>> Sifter::keynodeSetExtract(std::string fname, int
 	Sift *sdata;
 	sdata = new Sift();
 	this->runSift(pgmFile,sdata);
+
+	// so this is confusing and I probably should change it
+	//get P returns 4 float K points
+	// get K returns descriptors
 	std::vector<std::vector<float>> myp = sdata->getP();
 	std::vector<std::vector<int>> myk = sdata->getK();
 
@@ -46,24 +52,191 @@ std::vector<std::vector<float>> Sifter::keynodeSetExtract(std::string fname, int
 	{
 		targetL = 128;
 	}
-	for(int i = targetK; i > 0; i--)
+	
+	float ratioCheck = targetK / myp.size();
+	bool justTop = false;
+	if(ratioCheck > 0.25)
 	{
-		std::vector<float> row;
+		// just return top sizes
+		justTop = true;
+	}
 
-		for(int j = 0; j < 4; j++)
+	int numFor25 = 0.25* myp.size(); // the number of our "top 25%"
+	if(justTop == true)
+	{
+		numFor25 = targetK;
+	}
+	std::vector<int> candidates; // contains the indexes of our candidates for being a k, node.  These will eventually be the 
+	
+	// points that get clustered
+
+
+	
+	typedef std::map<float, int> MapType;
+	MapType my_map;	
+	for(int i = 0; i < myp.size(); i++)
+	{
+		my_map.insert(std::pair<float,int>(myp[i][2],i));
+	}
+	MapType::iterator iter = my_map.begin();
+	int i = 0;
+	for(MapType::const_iterator end = my_map.end(); end != iter; --end)
+	{
+		if(end->second > 0)
 		{
-			row.push_back(myp[i][j]);
+			if(i < numFor25)
+			{
+				candidates.push_back(end->second);
+				i++;
+			}
 		}
-		for(int j = 0; j < targetL; j++)
-		{
-			row.push_back(myk[i][j]);
-		}
-		pruned.push_back(row);
+	}
+	std::vector<int> final;
+
+	if(justTop == false)
+	{
+		final = this->kMeans(targetK,candidates,myp);
+	}
+	else
+	{
+		final = candidates;
 	}
 	
+	std::vector<std::vector<float>> prunedP;
+	std::vector<std::vector<int>> prunedL;
+
+	for(int i = 0; i < final.size(); i++)
+	{
+		prunedP.push_back(myp[final[i]]);
+		prunedL.push_back(myk[final[i]]);
+	}
+	
+
+	// for example, printing all descriptors
+	for(int i = 0; i < prunedL.size(); i++)
+	{
+		for(int j = 0; j < prunedL[i].size(); j++)
+		{
+			std::cout << prunedL[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+
+	// ACE - Prune each L here 
+	// pruned L contains k vectors of descriptors; this k is our selected k
+
+
+	
+	// combine rows of P and L and return
+
 	return pruned;
 }
 
+std::vector<int> Sifter::kMeans(int targetK, std::vector<int> candidates, std::vector<std::vector<float>> data)
+{
+	std::vector<std::vector<int>> member;
+	std::vector<std::vector<float>> centroids;
+
+	//initialize our members, just to top k
+	for(int i = 0; i < targetK; i++)
+	{
+		std::vector<int> temp;
+		temp.push_back(candidates[i]);
+		member.push_back(temp);
+		std::vector<float> ftemp;
+		ftemp.push_back(0);
+		ftemp.push_back(0);
+		centroids.push_back(ftemp);
+	}
+
+	// initialize centroid vector
+	
+
+	// let's just start with 5...
+	for(int i = 0; i < 5; i++)
+	{
+		//re compute centroid
+		//for every cluster
+		for(int j = 0; j < centroids.size(); j++)
+		{
+			std::vector<int> c = member[j]; // get members in this cluster
+			float x = 0;
+			float y = 0;
+			for(int k = 0; k < c.size(); k++)
+			{
+				x += data[c[k]][0]; // add x value for each member of cluster
+				y += data[c[k]][1]; // add y value for each member of cluster
+			}
+			x = x / c.size();
+			y = y / c.size();
+			centroids[j][0] = x;
+			centroids[j][1] = y;
+		}
+		// now we clear out our memberships
+		for(int j = 0; j < member.size(); j++)
+		{
+			member[j].clear();
+		}
+		// now we re-assign membership
+		for(int j = 0; j < candidates.size(); j++)
+		{
+			int myCluster;
+			myCluster = this->getClosestCentroid(centroids,data[candidates[j]][0],data[j][1]);
+			member[myCluster].push_back(candidates[j]);
+		}
+	}
+
+	std::vector<int> final;
+	// now we find the members closest to their centroids
+	for(int i = 0; i < centroids.size(); i++)
+	{
+		float distance = 10000;
+		int winner = 0;
+		for(int j = 0; j < member[i].size(); j++)
+		{
+			float d = this->distance(centroids[i][0],centroids[i][1],data[member[i][j]][0],data[member[i][j]][1]);
+			if(d < distance)
+			{
+				distance = d;
+				winner = member[i][j];
+			}
+		}
+		final.push_back(winner);
+	}
+	
+	return final;
+}
+
+int Sifter::getClosestCentroid(std::vector<std::vector<float>> cent,float x,float y)
+{
+	//first assume closest centroid is 0
+	int closest = 0;
+	float distance = this->distance(x,y,cent[0][0],cent[0][1]);
+
+	//then see if another one is clsoer
+	for(int i =1; i < cent.size(); i++)
+	{
+		float checkD = this->distance(x,y,cent[i][0],cent[i][1]);
+		if(checkD < distance)
+		{
+			distance = checkD;
+			closest = i;
+		}
+	}
+
+	return closest;
+}
+
+float Sifter::distance(float x1, float y1, float x2, float y2)
+{
+	float d;
+	float xd = (x1 - x2);
+	xd = xd * xd;
+	float xy = (y1 - y2);
+	xy = xy * xy;
+	d = sqrt(xd + xy);
+	return d;
+}
 
 void Sifter::runSift(std::string input, Sift *sdata)
 {
--- part2-sift/part2-sift/Sifter.h
+++ part2-sift/part2-sift/Sifter.h
@@ -9,5 +9,8 @@ public:
 	void runSift (std::string, Sift *);
 	std::vector<std::vector<float>> keynodeSetExtract(std::string, int, int);
 	void Sifter::getSortedIndices(std::vector<std::vector<float>>, std::vector<int>, int, int);
+	std::vector<int> Sifter::kMeans(int, std::vector<int>, std::vector<std::vector<float>>);
+	int Sifter::getClosestCentroid(std::vector<std::vector<float>>,float,float);
+	float Sifter::distance(float, float, float, float);
 };
 
